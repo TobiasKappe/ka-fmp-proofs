16f104658dfdd783384f66506d805e5baef8c68d
Tobias Kappé <tobias@kap.pe>
2022-09-09 16:02:50 +0200

    Prove additional axioms of weak KA.

--- proof.v
+++ proof.v
@@ -6862,3 +6862,229 @@ Proof.
       apply kleene_interp_witness_apply.
       intuition.
 Qed.
+
+Lemma term_equiv_free
+  (t1 t2: term)
+:
+  (forall w, term_matches t1 w <-> term_matches t2 w) <->
+  t1 == t2
+.
+Proof.
+  split; intros.
+  - now apply term_equiv_complete.
+  - now apply term_equiv_sound.
+Qed.
+
+Lemma starstar (a: A):
+  ($ a) * * == ($ a)*
+.
+Proof.
+  apply term_equiv_complete; split; intros.
+  - apply term_matches_star_split in H0.
+    apply term_matches_star_split.
+    destruct H0 as [l [? ?]]; subst.
+    induction l.
+    + now exists nil.
+    + destruct IHl as [l' [? ?]].
+      * intros.
+        apply H1.
+        now right.
+      * specialize (H1 a0 ltac:(now left)).
+        apply term_matches_star_split in H1.
+        destruct H1 as [l'' [? ?]]; subst.
+        exists (l'' ++ l').
+        rewrite concat_app, <- H0.
+        intuition.
+        apply in_app_iff in H1.
+        destruct H1; intuition.
+  - replace w with (w ++ nil) by (now rewrite app_nil_r).
+    apply MatchStarStep; auto.
+    apply MatchStarBase.
+Qed.
+
+Equations term_repeat
+  (n: nat)
+  (t: term)
+:
+  term
+:= {
+  term_repeat 0%nat t := 1;
+  term_repeat (S n) t := t ;; term_repeat n t;
+}.
+
+Lemma term_matches_star_repeat
+  (t: term)
+  (w: list A)
+:
+  term_matches (t*) w <->
+  exists n, term_matches (term_repeat n t) w
+.
+Proof.
+  split; intros.
+  - dependent induction H0.
+    + exists 0%nat.
+      autorewrite with term_repeat.
+      apply MatchOne.
+    + destruct (IHterm_matches2 t eq_refl) as [n ?].
+      exists (S n).
+      autorewrite with term_repeat.
+      now apply MatchTimes.
+  - destruct H0 as [n ?].
+    revert w H0; induction n; intros;
+    autorewrite with term_repeat in H0.
+    + dependent destruction H0.
+      apply MatchStarBase.
+    + dependent destruction H0.
+      apply MatchStarStep; auto.
+Qed.
+
+Lemma slide (a b: A):
+  ($ a ;; $ b)* ;; $a == $a ;; ($ b ;; $ a)*
+.
+Proof.
+  apply term_equiv_complete; split; intros.
+  - dependent destruction H0.
+    apply term_matches_star_repeat in H0_.
+    destruct H0_ as [n ?].
+    revert w1 w2 H0 H0_0; induction n;
+    autorewrite with term_repeat; intros.
+    + dependent destruction H0.
+      rewrite app_nil_l, <- app_nil_r.
+      apply MatchTimes; auto.
+      apply MatchStarBase.
+    + dependent destruction H0.
+      dependent destruction H0_.
+      repeat rewrite <- app_assoc.
+      apply MatchTimes; auto.
+      specialize (IHn _ _ H0_0 H0_3).
+      dependent destruction IHn.
+      rewrite <- x.
+      rewrite app_assoc.
+      * apply MatchStarStep; auto.
+        now apply MatchTimes.
+  - dependent destruction H0.
+    apply term_matches_star_repeat in H0_0.
+    destruct H0_0 as [n ?].
+    revert w1 w2 H0 H0_; induction n;
+    autorewrite with term_repeat; intros.
+    + dependent destruction H0.
+      rewrite app_nil_r,  <- app_nil_l.
+      apply MatchTimes; auto.
+      apply MatchStarBase.
+    + dependent destruction H0.
+      dependent destruction H0_.
+      specialize (IHn _ _ H0_0 H0_2).
+      dependent destruction IHn.
+      repeat rewrite <- app_assoc.
+      rewrite <- x.
+      repeat rewrite app_assoc.
+      apply MatchTimes; auto.
+      apply MatchStarStep; auto.
+      now apply MatchTimes.
+Qed.
+
+Lemma denest_pre (a b: A):
+  ($ a)* ;; ($b ;; ($ a)*)* <= ($ a + $ b)*
+.
+Proof.
+  apply EFixLeft.
+  apply term_lequiv_split.
+  - rewrite EStarLeft at 2.
+    rewrite EStarLeft at 3.
+    apply term_lequiv_split_left.
+    fold_term_lequiv.
+    apply times_mor_mono.
+    + unfold term_lequiv.
+      apply term_lequiv_split_left.
+      apply term_lequiv_refl.
+    + apply term_lequiv_refl.
+  - rewrite <- ETimesUnitRight with (t := ($ b ;; ($ a)*)*).
+    apply EFixLeft.
+    apply term_lequiv_split.
+    + rewrite EStarLeft with (t := $a + $b).
+      apply term_lequiv_split_left.
+      fold_term_lequiv.
+      rewrite <- ETimesAssoc.
+      apply times_mor_mono.
+      * unfold term_lequiv.
+        apply term_lequiv_split_right.
+        apply term_lequiv_refl.
+      * rewrite EDistributeLeft.
+        apply term_lequiv_split.
+        -- apply EFixLeft.
+           apply term_lequiv_split.
+           ++ rewrite EStarLeft with (t := $a + $b) at 2.
+              rewrite EStarLeft with (t := $a + $b) at 3.
+              apply term_lequiv_split_left.
+              fold_term_lequiv.
+              apply times_mor_mono.
+              ** apply term_lequiv_split_left.
+                 apply term_lequiv_refl.
+              ** apply term_lequiv_refl.
+           ++ rewrite EStarLeft with (t := $a + $b) at 2.
+              rewrite EStarLeft with (t := $a + $b) at 3.
+              apply term_lequiv_split_left.
+              apply term_lequiv_refl.
+        -- apply EFixLeft.
+           apply term_lequiv_split.
+           ++ rewrite EStarLeft with (t := $a + $b) at 2.
+              rewrite EStarLeft with (t := $a + $b) at 3.
+              apply term_lequiv_split_left.
+              fold_term_lequiv.
+              apply times_mor_mono.
+              ** apply term_lequiv_split_left.
+                 apply term_lequiv_refl.
+              ** apply term_lequiv_refl.
+           ++ rewrite EStarLeft with (t := $a + $b).
+              apply term_lequiv_split_right.
+              apply term_lequiv_refl.
+    + rewrite EStarLeft with (t := $a + $b).
+      apply term_lequiv_split_right.
+      apply term_lequiv_refl.
+Qed.
+
+Lemma term_lequiv_sound
+  (t1 t2: term)
+  (w: list A)
+:
+  t1 <= t2 ->
+  term_matches t1 w ->
+  term_matches t2 w
+.
+Proof.
+  intros.
+  apply term_equiv_sound with (t2 := t1 + t2).
+  + now symmetry.
+  + now apply MatchPlusLeft.
+Qed.
+
+Lemma denest (a b: A):
+  ($ a + $ b)* == ($ a)* ;; ($b ;; ($ a)*) *
+.
+Proof.
+  apply term_equiv_complete; split; intros.
+  - apply term_matches_star_repeat in H0.
+    destruct H0 as [n ?].
+    revert w H0; induction n;
+    autorewrite with term_repeat; intros.
+    + dependent destruction H0.
+      rewrite <- app_nil_l.
+      apply MatchTimes; apply MatchStarBase.
+    + dependent destruction H0.
+      dependent destruction H0_.
+      * apply IHn in H0_0.
+        dependent destruction H0_0.
+        rewrite app_assoc.
+        apply MatchTimes; auto.
+        apply MatchStarStep; auto.
+      * apply IHn in H0_0.
+        dependent destruction H0_0.
+        rewrite app_assoc.
+        rewrite <- app_nil_l.
+        apply MatchTimes; [apply MatchStarBase|].
+        apply MatchStarStep; auto.
+        apply MatchTimes; auto.
+  - eapply term_lequiv_sound.
+    + apply denest_pre.
+    + auto.
+Qed.
