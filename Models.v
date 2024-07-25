f6c7c79475e138ab9fdb7b561cf5df723165be2b
Tobias Kappé <tobias@kap.pe>
2024-07-25 16:25:52 +0200

    Define star-continuity and prove it for relational models.

--- Models.v
+++ Models.v
@@ -20,6 +20,17 @@ Section RelationalModels.
       R2 x'' x'
   .
 
+  Fixpoint monoid_relational_repeat
+    {X: Type}
+    (R: X -> X -> Prop)
+    (n: nat)
+  :=
+    match n with
+    | 0 => monoid_relational_unit
+    | S n => monoid_relational_compose R (monoid_relational_repeat R n)
+    end
+  .
+
   Program Definition monoid_relational (X: Type): monoid (X -> X -> Prop) := {|
     monoid_unit := monoid_relational_unit;
     monoid_compose := monoid_relational_compose;
@@ -57,6 +68,39 @@ Section RelationalModels.
         kleene_relational_star R x x''
   .
 
+  Definition kleene_relational_repeat
+    {X: Type}
+    (R: X -> X -> Prop)
+    (x x': X)
+  :
+    Prop
+  :=
+    exists n, monoid_relational_repeat R n x x'
+  .
+
+  Lemma kleene_relational_star_is_repeat
+    {X: Type}
+    (R: X -> X -> Prop)
+  :
+    kleene_relational_star R = kleene_relational_repeat R
+  .
+  Proof.
+    extensionality x; extensionality x'.
+    apply propositional_extensionality; split; intros.
+    - unfold kleene_relational_repeat.
+      induction H.
+      + now exists 0.
+      + destruct IHkleene_relational_star as [n ?].
+        now exists (S n), x'.
+    - destruct H as [n ?].
+      revert x x' H; induction n; intros.
+      + simpl in H.
+        unfold monoid_relational_unit in H; subst.
+        apply KleeneRelationalStarBase.
+      + destruct H as [? [? ?]].
+        eapply KleeneRelationalStarStep; intuition.
+  Qed.
+
   Definition kleene_relational_plus
     {X: Type}
     (R1 R2: X -> X -> Prop)
@@ -73,7 +117,15 @@ Section RelationalModels.
     kleene_relational_plus R1 R2 = R2
   .
   Proof.
-  Admitted.
+    split; intros.
+    - unfold kleene_relational_plus.
+      extensionality x; extensionality x'.
+      apply propositional_extensionality.
+      firstorder.
+    - rewrite <- H.
+      unfold kleene_relational_plus.
+      intuition.
+  Qed.
 
   Definition kleene_relational_zero
     {X: Type}
@@ -156,3 +208,55 @@ Section RelationalModels.
       intuition.
   Qed.
 End RelationalModels.
+
+Section StarContinuity.
+  Fixpoint kleene_power
+    {X: Type}
+    (K: kleene_algebra X)
+    (x: X)
+    (n: nat)
+  :
+    X
+  :=
+    match n with
+    | 0 => kleene_unit K
+    | S n => kleene_multiply K x (kleene_power K x n)
+    end
+  .
+
+  Definition kleene_star_continuous
+    {X: Type}
+    (K: kleene_algebra X)
+  :=
+    forall x y u v,
+      (forall n, kleene_plus K (kleene_compose K u (kleene_compose K (kleene_power K x n) v)) y = y) ->
+      kleene_plus K (kleene_compose K u (kleene_compose K (kleene_star K x) v)) y = y
+  .
+
+  Lemma kleene_power_is_relational_repeat
+    {X: Type}
+    (R: X -> X -> Prop)
+    (n: nat)
+  :
+    monoid_relational_repeat R n =
+    kleene_power (kleene_algebra_relational X) R n
+  .
+  Proof.
+    induction n; simpl.
+    - reflexivity.
+    - now rewrite IHn.
+  Qed.
+
+  Lemma kleene_relational_star_continuous (X: Type):
+    kleene_star_continuous (kleene_algebra_relational X)
+  .
+  Proof.
+    unfold kleene_star_continuous; intros R; intros.
+    simpl kleene_star; rewrite kleene_relational_star_is_repeat.
+    apply kleene_relational_plus_containment; intros.
+    destruct H0 as [x1 [? [x2 [[n ?] ?]]]].
+    rewrite <- (H n); left.
+    repeat (eexists; intuition; eauto).
+    now rewrite <- kleene_power_is_relational_repeat.
+  Qed.
+End StarContinuity.
